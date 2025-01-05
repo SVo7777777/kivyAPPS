ce9960ef56cfb971ae8c5a716a7c584386186c87
SVo7777777 <158193082+SVo7777777@users.noreply.github.com>
2025-01-05 19:46:03 +0300

    Add files via upload

--- календарь.py
+++ календарь.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 import datetime
 from datetime import timedelta
 from kivy.uix.boxlayout import BoxLayout
@@ -35,9 +36,15 @@ class CalendarLayout(BoxLayout):
         self. size_hint=(1, 1)
         self.layoutcalendar = BoxLayout(orientation="vertical", padding=0, size_hint=(1, 1))
         self.layoutgr= GridLayout(cols=7, spacing=0, size_hint=(1, 1))
-        layoutdata = BoxLayout(orientation="vertical", padding=0, size_hint=(1, .1))
+        layoutdata = BoxLayout(orientation="vertical", padding=0, size_hint=(1, .06))
         layoutdata1 = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .1))
-        
+        addbutton = Button(text='внести изменения', background_color= [.6,0,0.5,1],background_normal='',  font_size=40, size_hint=(1, 1))
+        sum = LabelX(text='всего часов: 300', color =[1,0,0,1], halign = 'right', font_size=50, size_hint=(1, 1))
+        sum.set_bgcolor(0,1,1,.8)
+        layoutadd_sum = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .09))
+        layoutadd_sum.add_widget(addbutton)
+        layoutadd_sum.add_widget(sum)
+        self.btnbottom = Button(text='', font_size=40, size_hint=(1, .06), background_normal='' , background_color= [.6,0,0.5,1] )
         current_year = datetime.datetime.now().year       
         days = monthrange(current_year, 3) [1]
         print(current_year)
@@ -73,23 +80,25 @@ class CalendarLayout(BoxLayout):
         #self.month_choose = False      
         
         self.but = {}
-        
+        self.entry = {}
+        self.h_layout2 = {}
+        self.lay1layout = {}  
         dropdown = DropDown(size_hint_y=1, size_hint_x=1, height=44, width=100)
         dropdownyear = DropDown(size_hint_y=1, size_hint_x=1, height=44, width=100)
         for index in self.month:                 
-            btn = Button(text=index, size_hint_y=None, size_hint_x=None, font_size=40, height=64, width=200)
+            btn = Button(text=index, size_hint_y=None, size_hint_x=None, font_size=40, height=64, width=200, background_normal='', background_color= [.6,0,0.2,1] )
             btn.bind(on_release=lambda btn: (dropdown.select(btn.text), self.vivod_calendar()))#, vivod_sotrudnikov()))      
             dropdown.add_widget(btn)               
-        self.mainbutton = Button(text = mon, font_size=40, size_hint=(1, 1)) #pos_hint: {"top": 1})                
+        self.mainbutton = Button(text = mon, background_normal='', background_color= [.6,0,0.2,1] , font_size=40, size_hint=(1, 1)) #pos_hint: {"top": 1})                
         self.mainbutton.bind(on_release=lambda btn: dropdown.open(btn)) 
         dropdown.bind(on_select=lambda instance, x: setattr(self.mainbutton, 'text', x))  
         
         years = [int(y)-2, int(y)-1, int(y), int(y)+1, int(y)+2, int(y)+3, int(y)+4]        
         for year in years:
-            btn1= Button(text=str(year), size_hint_y=None, size_hint_x=None, font_size=40,height=64, width=200)
+            btn1= Button(text=str(year), background_normal='', background_color= [.4,0,0.5,1] , size_hint_y=None, size_hint_x=None, font_size=40,height=64, width=200)
             btn1.bind(on_release=lambda btn1: (dropdownyear.select(btn1.text), self.vivod_calendar()))#, vivod_sotrudnikov()))
             dropdownyear.add_widget(btn1)
-        self.yearbutton = Button(text=y, font_size=40, size_hint=(1, 1))
+        self.yearbutton = Button(text=y, background_normal='', background_color= [.4,0,0.5,1] , font_size=40, size_hint=(1, 1))
         self.yearbutton.bind(on_release=lambda btn1: dropdownyear.open(btn1))
         dropdownyear.bind(on_select=lambda instance, x: setattr(self.yearbutton, 'text', x))  
         layoutdata1.add_widget(self.mainbutton)    
@@ -98,25 +107,33 @@ class CalendarLayout(BoxLayout):
         self.layoutcalendar.add_widget(self.layoutgr)
         self.vivod_calendar()
         self.add_widget(layoutdata)
+        self.add_widget(layoutadd_sum)
         self.add_widget(self.layoutcalendar)
+        self.add_widget(self.btnbottom)
             
     def clear(self):
         self.layoutcalendar.clear_widgets()
         self.layoutcalendar.add_widget(self.layoutgr)
-            
+        if self.h_layout2 != {}:
+            for day in self.days:  
+                self.layoutgr.remove_widget(self.lay1layout[day])
+            for i in range(1,43):
+                self.layoutgr.remove_widget(self.h_layout2[i])
+                
+    
     def vivod_calendar(self):     
         # Create the labels for the days of the week
         self.clear()
         day3 = 1
         year = self.yearbutton.text
         month = self.mainbutton.text
-        if year != 'выберите год' and  month != 'выберите месяц':          
-            #day = datetime.datetime(int('2024'), int(mo['декабрь']), i1+1)
-            #weekend = day.isoweekday()  
-            for day in self.days:
+        if year != 'выберите год' and  month != 'выберите месяц':                                
+            for day in self.days:           
+                self.lay1layout[day] =BoxLayout(orientation="horizontal",height=60, size_hint=(1, .5))     
                 lay1 = LabelX(halign="center", text=day, font_size=50, size_hint=(1, 1), color=[0, 0 ,0, 1])
                 lay1.set_bgcolor(1,1,1,.8)
-                self.layoutgr.add_widget(lay1)
+                self.lay1layout[day].add_widget(lay1)
+                self.layoutgr.add_widget(self.lay1layout[day])
     
             # Add buttons for each day of the month
             #day1 = datetime.datetime(int(year), int(month), 1)
@@ -129,11 +146,17 @@ class CalendarLayout(BoxLayout):
             den_nedely = self.day_week[str(weekend1)]
             print(den_nedely)
             for day in range(1, 43 ):
-                self.but[day] = Button(text='' , border = [10, 10, 10, 10], background_color= [1,1,1,1], font_size = 65) 
-                self.but[day].background_normal=''     
+                self.h_layout2[day] = BoxLayout(orientation="vertical",height=60, size_hint=(1, 1))
+                self.entry[day] = TextInput(halign="center", cursor_color=[0, 0, 1, 1], font_size=40,
+                                                     size_hint=(1, 1), foreground_color=[1,0,0,1], width=50, multiline=False)      
+                self.but[day] = LabelX(text='' ,  font_size = 65) 
+                self.but[day].set_bgcolor(1,1,1,1) 
                 #self.but[day].background_down=''
-                self.layoutgr.add_widget(self.but[day])        
-                if day < weekend1:                                      
+                self.h_layout2[day].add_widget(self.but[day])
+                self.h_layout2[day].add_widget(self.entry[day])   
+                self.layoutgr.add_widget(self.h_layout2[day])       
+                if day < weekend1:        
+                    self.h_layout2[day].remove_widget(self.entry[day])                              
                     print(c)
                     today = datetime.datetime.now()
                     first = today.replace(day=1)
@@ -150,6 +173,7 @@ class CalendarLayout(BoxLayout):
                     c -= 1                                              
                 else:
                     try:
+                         
                         day1 = datetime.datetime(int(year), int(self.mo[month]), day-weekend1+1)
                         now = datetime.datetime.now()
                         data = now.strftime("%d ")
@@ -160,11 +184,13 @@ class CalendarLayout(BoxLayout):
                         weekend = day1.isoweekday()  
                         print(str(day-weekend1+1)+self.day_week[str(weekend)] )
                         if weekend == 6 or weekend == 7:                                                     
-                            self.but[day].background_color= [.6,0,0.5,1]       
+                            self.but[day].set_bgcolor(.4,.3,0,1)
+                            self.entry[day].background_color= [.6,0,0.5,1]
+                            self.entry[day].foreground_color = [0,1 ,1, 1]
                             self.but[day].text= str(day-weekend1+1)#+self.day_week[str(weekend)]        
                         else:   
                             self.but[day].color = [0, 0 ,0, 1]
-                            self.but[day].background_color= [.8,.8,0,1]
+                            self.but[day].set_bgcolor(.9,0,0.9,1)
                             self.but[day].text= str(day-weekend1+1)#+self.day_week[str(weekend)] 
                         print('мы тут')
                        
@@ -172,15 +198,18 @@ class CalendarLayout(BoxLayout):
                             print('совпало')
                             self.but[day].background_color= [0,0,1,.8]
                             
-                    except ValueError:                        
+                    except ValueError:    
+                        self.h_layout2[day].remove_widget(self.entry[day])                    
                         print('ValueError')                       
                         self.but[day].text=str(day3)
                         self.but[day].color = [0, 0 ,0, .2]
                         day3 += 1
                         if day == 34 or day == 35:
-                            self.but[day].background_color= [1,1,1,.8]
+                            self.but[day].set_bgcolor(1,1,1,.8)
                         if day == 41 or day == 42:
-                            self.but[day].background_color= [1,1,1,.8]
+                            self.but[day].set_bgcolor(1,1,1,.8)
+                            
+                   
                             
                     
                     
