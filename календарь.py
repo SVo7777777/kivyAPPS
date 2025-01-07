df112c498344bafa2e8a31b7f232237892844441
SVo7777777 <158193082+SVo7777777@users.noreply.github.com>
2025-01-07 19:22:14 +0300

    Add files via upload

--- календарь.py
+++ календарь.py
@@ -34,24 +34,27 @@ class CalendarLayout(BoxLayout):
         col_day = { '01':31, '02':28}
         self.mo= { 'январь':'1','февраль':'2', 'март':'3', 'апрель':'4',  'май':'6',  'июнь':'6', 'июль':'7',  'август':'8', 'сентябрь':'9','октябрь':'10', 'ноябрь':'11', 'декабрь':'12'}   
         self.day_week = {'1':'пн', '2':'вт','3':'ср','4':'чт','5':'пт','6':'сб','7':'вс'}
-        self.days = {0:'пн', 1:'вт', 2:'ср', 3:'чт', 4:'пт', 5:'сб', 6:'вс'}
+        self.days = {'0':'пн', '1':'вт', '2':'ср', '3':'чт', '4':'пт', '5':'сб', '6':'вс'}
         self.month = ['январь', 'февраль','март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
         self.orientation="vertical"
         self.padding=0
         self. size_hint=(1, 1)
         self.layoutcalendar = BoxLayout(orientation="vertical", padding=0, size_hint=(1, 1))
         self.layoutgr= GridLayout(cols=7, spacing=0, size_hint=(1, 1))
-        layoutdata = BoxLayout(orientation="vertical", padding=0, size_hint=(1, .06))
-        layoutdata1 = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .1))
+        self.layoutweek= GridLayout(cols=7, spacing=0, size_hint=(1, .057))
+        layoutdata = BoxLayout(orientation="vertical", padding=0, size_hint=(1, .1))
+        layoutdata1 = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .2))
         addbutton = Button(text='внести изменения', background_color= [.6,0,0.5,1],background_normal='',  font_size=40, size_hint=(1, 1))
         
         self.sumprint = LabelX(color =[1,0,0,1], halign = 'right', font_size=50, size_hint=(1, 1))
         self.sumprint.set_bgcolor(1,0,1,.8)
-        self.sumprint.text='всего часов: '
-        layoutadd_sum = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .09))
+        
+        self.sumprint.text='всего часов: '+'\n'
+        layoutadd_sum = BoxLayout(orientation="horizontal", padding=0, size_hint=(1, .2))
         layoutadd_sum.add_widget(addbutton)
         layoutadd_sum.add_widget(self.sumprint)
-        self.btnbottom = Button(text='', font_size=40, size_hint=(1, .06), background_normal='' , background_color= [.6,0,0.5,1] )
+        
+        self.btnbottom = Button(text='', font_size=40, size_hint=(1, .2), background_normal='' , background_color= [1,1,1,1] )
         current_year = datetime.datetime.now().year       
         days = monthrange(current_year, 3) [1]
         print(current_year)
@@ -96,7 +99,7 @@ class CalendarLayout(BoxLayout):
         dropdownyear = DropDown(size_hint_y=1, size_hint_x=1, height=44, width=100)
         for index in self.month:                 
             btn = Button(text=index, size_hint_y=None, size_hint_x=None, font_size=40, height=64, width=200, background_color=[.8,0,.8,1] )
-            btn.bind(on_release=lambda btn: (dropdown.select(btn.text), self.vivod_calendar()))#, vivod_sotrudnikov()))      
+            btn.bind(on_release=lambda btn: (dropdown.select(btn.text), self.vivod_calendar(), self.vivod_chasov()))      
             dropdown.add_widget(btn)               
         self.mainbutton = Button(text = mon,  background_color= [.8,0,.8,1] , font_size=40, size_hint=(1, 1)) #pos_hint: {"top": 1})                
         self.mainbutton.bind(on_release=lambda btn: dropdown.open(btn)) 
@@ -105,7 +108,7 @@ class CalendarLayout(BoxLayout):
         years = [int(y)-2, int(y)-1, int(y), int(y)+1, int(y)+2, int(y)+3, int(y)+4]        
         for year in years:
             btn1= Button(text=str(year), background_color= [.8,0,.8,1] , size_hint_y=None, size_hint_x=None, font_size=40,height=64, width=200)
-            btn1.bind(on_release=lambda btn1: (dropdownyear.select(btn1.text), self.vivod_calendar()))#, vivod_sotrudnikov()))
+            btn1.bind(on_release=lambda btn1: (dropdownyear.select(btn1.text), self.vivod_calendar(), self.vivod_chasov()))
             dropdownyear.add_widget(btn1)
         self.yearbutton = Button(text=y, background_color= [.8,0,.8,1] , font_size=40, size_hint=(1, 1))
         self.yearbutton.bind(on_release=lambda btn1: dropdownyear.open(btn1))
@@ -113,12 +116,20 @@ class CalendarLayout(BoxLayout):
         layoutdata1.add_widget(self.mainbutton)    
         layoutdata1.add_widget(self.yearbutton) 
         layoutdata.add_widget(layoutdata1) 
+        for day in range(7):           
+                self.lay1layout[day] =BoxLayout(orientation="horizontal",height=60, size_hint=(1, .5))     
+                lay1 = LabelX(halign="center", text=self.days[str(day)], font_size=50, size_hint=(1, 1), color=[0, 0 ,0, 1])
+                lay1.set_bgcolor(1,1,1,.8)
+                self.lay1layout[day].add_widget(lay1)
+                self.layoutweek.add_widget(self.lay1layout[day])
         self.layoutcalendar.add_widget(self.layoutgr)
         self.vivod_calendar()
-        self.vivod_chasov()
+        self.vivod_chasov()       
+        
         self.add_widget(layoutdata)
-        self.add_widget(layoutadd_sum)
+        self.add_widget(self.layoutweek)
         self.add_widget(self.layoutcalendar)
+        self.add_widget(layoutadd_sum)
         self.add_widget(self.btnbottom)
         hours = HoursAdd()
         addbutton.bind(on_press=lambda ad: self.addchanges())    
@@ -138,6 +149,7 @@ class CalendarLayout(BoxLayout):
         m_y =month + ' ' +year
         s = 0
         k = 0
+        h = []
         with open("tabel_.txt", "r") as f:
             lines = f.readlines()        
             print('vivod_calendar=', lines)     
@@ -150,18 +162,21 @@ class CalendarLayout(BoxLayout):
                         print(st)
                         print(st[2])
                         h = st[2].split('-')
-                        print(h)
-                        print(len(h))
+                        #print(h)
+                        #print(len(h))
         for i in range(self.first, self.first+self.days):     
-            self.entry[i].text=h[s]
-            s += 1
+            if h == []:
+                pass
+            else:
+                self.entry[i].text=h[s]
+                s += 1
             try:
                k= k+float(self.entry[i].text)   
                print(k)                                                            
             except ValueError:
                     pass  
         
-        self.sumprint.text='всего часов: '+str(k)
+        self.sumprint.text='всего часов: '+'\n'+str(k)
             
     def addchanges(self):
         print('self.first=', self.first)
@@ -178,7 +193,7 @@ class CalendarLayout(BoxLayout):
                       
             hours = hours+self.entry[i].text+'-'
             
-        self.sumprint.text='всего часов: '+str(k)
+        self.sumprint.text='всего часов: \n'+str(k)
         m_y =month + ' ' +year
         stroka_s_chasami=m_y+ ' '+hours
         print(stroka_s_chasami)
@@ -197,10 +212,7 @@ class CalendarLayout(BoxLayout):
                                     print(new_line)
                                 else:
                                     f1.write(line)       
-                                    print(line)                                  
-        
-        
-        
+                                    print(line)                                                     
     
     def vivod_calendar(self):     
         # Create the labels for the days of the week
@@ -212,12 +224,7 @@ class CalendarLayout(BoxLayout):
         
                         
         if year != 'выберите год' and  month != 'выберите месяц':                                
-            for day in range(7):           
-                self.lay1layout[day] =BoxLayout(orientation="horizontal",height=60, size_hint=(1, .5))     
-                lay1 = LabelX(halign="center", text=self.days[day], font_size=50, size_hint=(1, 1), color=[0, 0 ,0, 1])
-                lay1.set_bgcolor(1,1,1,.8)
-                self.lay1layout[day].add_widget(lay1)
-                self.layoutgr.add_widget(self.lay1layout[day])
+            
     
             # Add buttons for each day of the month
             #day1 = datetime.datetime(int(year), int(month), 1)
@@ -233,7 +240,7 @@ class CalendarLayout(BoxLayout):
                 
                 
                 self.h_layout2[day] = BoxLayout(orientation="vertical",height=60, size_hint=(1, 1))
-                self.entry[day] = TextInput(halign="center", cursor_color=[0, 0, 1, 1], font_size=40,
+                self.entry[day] = TextInput(halign="center", cursor_color=[0, 0, 1, 1], font_size=50,
                                                      size_hint=(1, 1), foreground_color=[1,0,0,1], width=50, multiline=False)      
                 self.but[day] = LabelX(text='' ,  font_size = 65) 
                 self.but[day].set_bgcolor(1,1,1,1) 
@@ -259,10 +266,7 @@ class CalendarLayout(BoxLayout):
                     self.but[day].color = [0, 0 ,0, .2]
                     c -= 1                                              
                 else:
-                    try:
-                        
-                            
-                        
+                    try:                                                                         
                         day1 = datetime.datetime(int(year), int(self.mo[month]), day-weekend1+1)
                         now = datetime.datetime.now()
                         data = now.strftime("%d ")
@@ -275,7 +279,7 @@ class CalendarLayout(BoxLayout):
                         if weekend == 6 or weekend == 7:                                                     
                             self.but[day].set_bgcolor(.4,.3,0,1)
                             self.entry[day].background_color= [.6,0,0.5,1]
-                            self.entry[day].foreground_color = [0,1 ,1, 1]
+                            self.entry[day].foreground_color = [1,1 ,1, 1]
                             self.but[day].text= str(day-weekend1+1)#+self.day_week[str(weekend)]        
                         else:   
                             self.but[day].color = [0, 0 ,0, 1]
